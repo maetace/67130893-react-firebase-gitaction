5bddecd6ac84a891c1a40012b79b7df4d96f0912
maetace <itpirain@gmail.com>
2025-05-03 13:45:35 +0700

    first commit

--- src/report.jsx
+++ src/report.jsx
@@ -62,7 +62,7 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div>Mr .Siwakorn Banluesapy</div>
+            <div>67130893 - Maetee Ninratana</div>
           </Container>
         </Navbar>
         <Container>
